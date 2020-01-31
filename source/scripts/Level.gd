1644c1e3aab1669a810c3a430843802411bbf721
Jan Forberger <jan.forberger@gmail.com>
2020-01-31 23:08:32 +0100

    Level win condition when the ship has reached it's destination

--- source/scripts/Level.gd
+++ source/scripts/Level.gd
@@ -1,8 +1,11 @@
 extends Node
 
+# How long is the journey in this level
 export (float) var length = 1000
-export (float) var speed = 10
+# How many steps per second shall the ship take
+export (float) var speed = 100
 
+# How many steps did the ship take so far
 var progress : float = 0
 
 
@@ -11,5 +14,11 @@ func _ready() -> void:
 
 
 func _process(delta : float) -> void:
+	# Advance the level progress
 	progress += speed * delta
 	$HUD.set_level_progress(progress / length)
+	# Check if level has been completed
+	if progress >= length:
+		set_process(false)
+		$HUD.show_win_screen()
+
--- source/scripts/hud/HUD.gd
+++ source/scripts/hud/HUD.gd
@@ -3,3 +3,15 @@ extends Control
 
 func set_level_progress(progress : float) -> void:
 	$LevelProgress.set_progress(progress)
+
+
+func show_win_screen() -> void:
+	$WinScreen.visible = true
+
+
+func _on_BtnReplay_button_up():
+	get_tree().reload_current_scene()
+
+
+func _on_BtnMainMenu_button_up():
+	print_debug("Main Menu not yet implemented")
