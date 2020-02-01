55add59fce7825812717fac18e80f4e9f9dab4da
zitroanen <tim.naetebus@gmail.com>
2020-02-01 14:18:27 -0500

    Updatet menu

--- source/Menues/Preferences.gd
+++ source/Menues/Preferences.gd
@@ -1,18 +1,37 @@
 extends Control
 
-var height
-var width
+var height := 0
+var width := 0
+var entry_full := false
+var entry_resize := false
 
-func set_fullscreen(entry):
-	ProjectSettings.set_setting("application/display/window/size/Fullscreen", str(entry))
+func set_fullscreen():
+	if entry_full == true:
+		entry_full = false
+	else:
+		entry_full = true
+		entry_resize = true
+		set_resizable()
+	OS.window_fullscreen = entry_full
 
-func set_resizable(entry):
-	ProjectSettings.set_setting("application/config/name", str(entry))
-	pass
+func set_resizable():
+	if entry_resize == true:
+		entry_resize = false
+	else:
+		entry_resize = true
+		entry_full = true
+		set_fullscreen()
+	OS.window_resizable = entry_resize
 
-func set_Res(height, width):
-	ProjectSettings.set_setting("application/config/name", "Example")
-	pass
+func set_Res():
+	if entry_full == true:
+		set_fullscreen()
+	elif entry_resize == true:
+		set_resizable()
+	if height > 0 && width > 0:
+		OS.window_size = Vector2(width, height)
+	else:
+		pass
 
 #Set Resolution scaling 
 func set_scaling(text):
@@ -30,24 +49,36 @@ func set_scaling(text):
 		$Grid/MarginContainer/if_4_3.visible = true
 
 func _on_Accept_pressed():
-	pass # Replace with function body.
-
+	if entry_full:
+		get_tree().change_scene("res://Menues/start_menu.tscn")
+	elif entry_resize:
+		get_tree().change_scene("res://Menues/start_menu.tscn")
+	set_Res()
+	get_tree().change_scene("res://Menues/start_menu.tscn")
 
 func _on_Cancel_pressed():
-	pass # Replace with function body.
-
+	get_tree().change_scene("res://Menues/start_menu.tscn")
 
 func _on_Resolution_selected(id):
 	set_scaling(str($Grid/Res.text))
 
-
 func _on_19_6_selected(id):
-	pass # Replace with function body.
-
+	var auswahl := str($Grid/MarginContainer/if_16_9.text)
+	width = int(auswahl.substr(0, auswahl.find("x")))
+	height = int(auswahl.substr(auswahl.find("x"), auswahl.length()-1))
 
 func _on_16_10_item_selected(id):
-	pass # Replace with function body.
-
-
+	var auswahl := str($Grid/MarginContainer/if_16_10.text)
+	width = int(auswahl.substr(0, auswahl.find("x")))
+	height = int(auswahl.substr(auswahl.find("x"), auswahl.length()-1))
+	
 func _on_4_3_item_selected(id):
-	pass # Replace with function body.
+	var auswahl := str($Grid/MarginContainer/if_4_3.text)
+	width = int(auswahl.substr(0, auswahl.find("x")))
+	height = int(auswahl.substr(auswahl.find("x"), auswahl.length()-1))
+
+func _on_Fullscreen_pressed():
+	set_fullscreen()
+
+func _on_Resizeable_pressed():
+	set_resizable()
