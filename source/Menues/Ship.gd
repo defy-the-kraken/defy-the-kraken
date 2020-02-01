fe8f9237f1e45184872988f5c4bd009f9a35c16c
zitroanen <tim.naetebus@gmail.com>
2020-02-01 15:52:28 +0100

    Menu update

--- source/Menues/Ship.gd
+++ source/Menues/Ship.gd
@@ -3,11 +3,18 @@ extends Sprite
 export var speed := 5
 var posi = Vector2()
 var on = true
+var rng = RandomNumberGenerator.new()
 
 func _process(Delta):
-	self.move_local_x(-speed)
+	move_local_x(-speed)
 
 func _on_Timer_timeout():
-	posi = self.position
-	self.position = posi
-	$Timer.start(10)
+	if (on == true):
+		posi = self.position
+		on = false
+	position = posi
+	speed = rng.randi_range(0, 5)
+	if speed > 1:
+		get_parent().get_node("Timer").start(7)
+	else:
+		get_parent().get_node("Timer").start(13)
