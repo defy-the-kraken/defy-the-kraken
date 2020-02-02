a44bbab58ec94f182a4f55fd21e0a6c03a6a5339
Jan Forberger <jan.forberger@gmail.com>
2020-02-02 12:09:19 +0100

    Player movement and movement animation

--- source/scripts/Player.gd
+++ source/scripts/Player.gd
@@ -1,9 +1,14 @@
 extends KinematicBody2D
 
+export (int) var walk_speed = 50
+export (int) var climb_speed = 30
+export (int) var gravity = 50
+
 var input_device : int = 0
-var can_climb : bool = false
-var has_material : bool = false
-var avail_interact = null
+var can_climb : bool = true
+var has_supplies : bool = false
+var avail_interact : Node2D = null
+var velocity : Vector2 = Vector2.ZERO
 
 enum {
 	MOVE_LEFT,
@@ -11,8 +16,9 @@ enum {
 	IDLE, 
 	CLIMB_UP, 
 	CLIMB_DOWN,
-	IDLE_CLIMB, 
-	PUMPING
+	CLIMB_IDLE,
+	PUMPING,
+	REPAIRING
 	}
 var state : int = IDLE
 
@@ -21,9 +27,6 @@ func _input(event : InputEvent) -> void:
 		return
 	# is it an activateion
 	if event.is_pressed():
-		# Discard all inputs while pumping
-		if state == PUMPING:
-			return
 		if event.is_action_pressed("interact"):
 			if avail_interact and avail_interact.has_method("interact"):
 				avail_interact.interact()
@@ -35,37 +38,52 @@ func _input(event : InputEvent) -> void:
 			change_state(MOVE_LEFT)
 		elif event.is_action_pressed("move_right"):
 			change_state(MOVE_RIGHT)
-			
 	# or is it a deactivation
 	else:
 		match state:
 			MOVE_LEFT:
-				if event.is_action_released("move_left"):
+				if event.is_action("move_left"):
 					change_state(IDLE)
 			MOVE_RIGHT:
-				if event.is_action_released("move_right"):
+				if event.is_action("move_right"):
 					change_state(IDLE)
 			CLIMB_UP:
-				if event.is_action_released("move_up"):
-					change_state(IDLE_CLIMB)
+				if event.is_action("move_up"):
+					change_state(CLIMB_IDLE)
 			CLIMB_DOWN:
-				if event.is_action_released("move_down"):
-					change_state(IDLE_CLIMB)
-			PUMPING:
-				if event.is_action_released("interact"):
-					change_state(IDLE)
-			_: change_state(IDLE)
-
+				if event.is_action("move_down"):
+					change_state(CLIMB_IDLE)
 
 func change_state(new_state : int) -> void:
 	# Check for actual state change
 	if new_state == state:
 		return
+	# save the state
 	state = new_state
+	# Switch animation
+	match state:
+		MOVE_LEFT:
+			$Sprite.animation = "walk"
+			$Sprite.flip_h = true
+			$Sprite.playing = true
+			print_debug("State changed: Move left")
+		MOVE_RIGHT:
+			$Sprite.animation = "walk"
+			$Sprite.flip_h = false
+			$Sprite.playing = true
+			print_debug("State changed: Move right")
+		IDLE:
+			$Sprite.animation = "idle"
+		CLIMB_UP, CLIMB_DOWN:
+			$Sprite.animation = "climb"
+			$Sprite.playing = true
+		CLIMB_IDLE:
+			$Sprite.animation = "climb"
+			$Sprite.playing = false
+		
 
 func supply() -> void:
-	has_material = true
-
+	has_supplies = true
 
 func enable_interaction(interaction : Node2D):
 	avail_interact = interaction
@@ -74,6 +92,20 @@ func disable_interaction(interaction : Node2D):
 	if avail_interact == interaction:
 		avail_interact = null
 
+func _physics_process(delta : float) -> void:
+	match state:
+		MOVE_LEFT:
+			velocity = Vector2(-walk_speed, gravity)
+		MOVE_RIGHT:
+			velocity = Vector2(walk_speed, gravity)
+		IDLE:
+			velocity = Vector2(0, gravity)
+		CLIMB_UP:
+			velocity = Vector2(0, -climb_speed)
+		CLIMB_DOWN:
+			velocity = Vector2(0, climb_speed)
+		_: velocity = Vector2.ZERO
+	move_and_slide(velocity)
 
 """
 func _physics_process(delta : float) -> void:
