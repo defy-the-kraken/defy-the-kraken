a82e08638788f64f99316bf5868093343927382f
Jan Forberger <jan.forberger@gmail.com>
2020-02-02 14:54:18 +0100

    Doors are almost working

--- source/Ship/Door.gd
+++ source/Ship/Door.gd
@@ -2,11 +2,12 @@ extends AnimatedSprite
 
 enum {CLOSED, OPEN}
 
-func interact(body : Player) -> void:
+func interact(body : Player) -> String:
 	if frame == CLOSED:
 		open()
 	else:
 		close()
+	return "door"
 
 func open() -> void:
 	frame = OPEN
@@ -16,6 +17,13 @@ func close() -> void:
 
 func _on_Body_body_entered(body : Player) -> void:
 	body.enable_interaction(self)
+	$InteractionHint.show()
+	if frame == OPEN:
+		body.set_collision_mask_bit(0, false)
+	else:
+		body.set_collision_mask_bit(0, true)
 
 func _on_Body_body_exited(body : Player) -> void:
 	body.disable_interaction(self)
+	$InteractionHint.hide()
+	body.set_collision_mask_bit(0, true)
